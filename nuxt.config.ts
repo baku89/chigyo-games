3e2c06ddd11c18bc12ca470bdeb740668bca653f
Baku Hashimoto <mail@baku89.com>
2025-08-26 20:49:57 +0900

    Install Pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@ import {defineNuxtConfig} from 'nuxt/config'
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	modules: ['@nuxt/eslint'],
+	modules: ['@nuxt/eslint', '@pinia/nuxt'],
 	compatibilityDate: '2024-11-01',
 	devtools: {enabled: true},
 	ssr: false,
